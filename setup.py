c6c1013443468dabb6e128a18618c337358c94fc
reethika125@gmail.com <reethika125@gmail.com>
2025-04-11 17:56:55 -0400

    first commit

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str) ->List[str]:
 
 setup(
     name='mlproject',
-    version='0.0.1',
+    version='0.0.2',
     author='Reethika Edupuganti',
     author_email='reethika125@gmail.com',
     packages=find_packages(),
