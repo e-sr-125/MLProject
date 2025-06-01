96e4fc68728f0b334c12c8d18b3578df0308ebc2
reethika125@gmail.com <reethika125@gmail.com>
2025-06-01 17:50:10 -0400

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,7 +13,6 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
-
 from src.exception import CustomException
 from src.logger import logging
 
